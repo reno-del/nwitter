18e4a873592333a885d2eec93b7238c5c52e577f
reno-del <85153521+reno-del@users.noreply.github.com>
2023-07-12 14:03:59 +0900

    3.1

--- src/routeset/Home.js
+++ src/routeset/Home.js
@@ -1,9 +1,22 @@
+import { dbService } from "fbase";
+import { addDoc, collection } from "firebase/firestore";
 import React, { useState } from "react";
 
 const Home = () => {
   const [nweet, setNweet] = useState("");
-  const onSubmit = (event) => {
-    event.prventDefault();
+  const onSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const docRef = await addDoc(collection(dbService, "nweets"), {
+        nweet,
+        createdAt: Date.now(),
+      });
+      console.log("Document written with ID: ", docRef.id);
+    } catch (error) {
+      console.error("Error adding document: ", error);
+    }
+
+    setNweet("");
   };
   const onChange = (event) => {
     const {
@@ -13,7 +26,7 @@ const Home = () => {
   };
   return (
     <div>
-      <form>
+      <form onSubmit={onSubmit}>
         <input
           value={nweet}
           onChange={onChange}
@@ -21,7 +34,7 @@ const Home = () => {
           placeholder="What's on your mind?"
           maxLength={120}
         />
-        <input type="submit" value="Ntweet" />
+        <input type="submit" value="Nweet" />
       </form>
     </div>
   );
